e521fb847c464e8a76e95ef8e7cb5b40e598c2e0
Kloda, Peter <peter.kloda@cuw.edu>
2019-02-27 19:08:41 -0600

    program 5

--- app/src/main/java/com/example/baller/BallerAddNewActivity.java
+++ app/src/main/java/com/example/baller/BallerAddNewActivity.java
@@ -4,9 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class BallerAddNewActivity extends AppCompatActivity {
 
